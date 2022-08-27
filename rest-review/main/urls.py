02cd3810250b252286cbb0f4700ed6c5ce238ce8
Ethan Berrett <ethanb@qualtrics.com>
2022-08-27 14:12:33 -0500

    edit adding new line, not working correctly

--- rest-review/main/urls.py
+++ rest-review/main/urls.py
@@ -4,6 +4,7 @@ from . import views  #importing our view file
 urlpatterns = [
     path("", views.homepage, name="home"), # mapping the homepage function
     path("add-rest", views.add_rest, name = "add-rest"),
-    path("my-rests", views.show_rest, name = "my-rests"), 
+    path("edit-rest/<int:id>/", views.edit_rest, name = "edit-rest"), 
+    path("my-rests", views.show_rest, name = "my-rests")
 ]
 
--- rest-review/main/views.py
+++ rest-review/main/views.py
@@ -1,25 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import AddRest
 from .models import Rest
 from django.contrib.auth.decorators import login_required
 
+
 def homepage(request):
-  return render(request, 'index.html', context={})
+    return render(request, 'index.html', context={})
+
 
 @login_required(login_url='/accounts/login')
 def add_rest(request):
     if request.method == 'POST':
-        form = AddRest(request.POST, user=request.user)
+        form = AddRest(request.POST)
+        #form = form_object.save(commit=False)
         if form.is_valid():
-            form.save()
+            obj = form.save(commit=False)
+            obj.user = request.user
+            obj.save()
         return HttpResponseRedirect('my-rests')
     else:
-        form = AddRest(user=request.user)
-        
+        form = AddRest()
 
     return render(request, 'add-rest.html', {'form': form})
+
+
+@login_required(login_url='/accounts/login')
+def edit_rest(request, **kwargs):
+    id = kwargs['id']
+    obj = get_object_or_404(Rest, id=id)
+    if request.method == 'POST':
+        form = AddRest(request.POST, instance=obj)
+        form.user = request.user
+        if form.is_valid():
+            print("HERE HERE HERE")
+            obj = form.save(commit=False)
+            obj.save()
+        return HttpResponseRedirect('my-rests')
+    else:
+        form = AddRest(instance=obj)
+
+    return render(request, 'edit-rest.html', {'form': form})
+
 
 @login_required(login_url='/accounts/login')
 def show_rest(request):
