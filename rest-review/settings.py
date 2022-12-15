8f46303cce063ab40308396eebd7947cfa24aaea
Ethan Berrett <ethanb@qualtrics.com>
2022-12-14 19:52:05 -0600

    setup mobile styling

--- rest-review/settings.py
+++ rest-review/settings.py
@@ -26,11 +26,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-TESTING = False
+
+TESTING = True
 
 if TESTING:
+    DEBUG = True
+else:
     DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0']
